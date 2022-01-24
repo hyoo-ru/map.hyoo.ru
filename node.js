1f5f0bf8a888c92c5ddd4702ced63ec565b9b6cc
nin-jin <nin-jin@users.noreply.github.com>
2022-01-24 07:21:41 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -4838,6 +4838,9 @@ var $;
 var $;
 (function ($) {
     class $mol_paragraph extends $mol_view {
+        dom_name() {
+            return "p";
+        }
         line_height() {
             return 24;
         }
@@ -4856,6 +4859,13 @@ var $;
     $.$mol_paragraph = $mol_paragraph;
 })($ || ($ = {}));
 //mol/paragraph/-view.tree/paragraph.view.tree.ts
+;
+"use strict";
+var $;
+(function ($) {
+    $mol_style_attach("mol/paragraph/paragraph.view.css", "[mol_paragraph] {\n\tmargin: 0;\n}\n");
+})($ || ($ = {}));
+//mol/paragraph/-css/paragraph.view.css.ts
 ;
 "use strict";
 var $;
@@ -7866,6 +7876,9 @@ var $;
 var $;
 (function ($) {
     class $mol_page extends $mol_view {
+        dom_name() {
+            return "article";
+        }
         sub() {
             return [
                 this.Head(),
@@ -7898,6 +7911,7 @@ var $;
         Head() {
             const obj = new this.$.$mol_view();
             obj.minimal_height = () => 64;
+            obj.dom_name = () => "header";
             obj.sub = () => this.head();
             return obj;
         }
@@ -7920,6 +7934,7 @@ var $;
         }
         Foot() {
             const obj = new this.$.$mol_view();
+            obj.dom_name = () => "footer";
             obj.sub = () => this.foot();
             return obj;
         }
