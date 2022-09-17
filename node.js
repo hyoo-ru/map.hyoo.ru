ab5b576553a68651437b77e808cb35cb5881a064
nin-jin <nin-jin@users.noreply.github.com>
2022-09-17 07:13:26 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -4556,7 +4556,10 @@ var $;
         selection(val) {
             if (val !== undefined)
                 return val;
-            return [];
+            return [
+                0,
+                0
+            ];
         }
         auto() {
             return [
@@ -4572,7 +4575,8 @@ var $;
                 spellcheck: this.spellcheck(),
                 autocomplete: this.autocomplete_native(),
                 selectionEnd: this.selection_end(),
-                selectionStart: this.selection_start()
+                selectionStart: this.selection_start(),
+                inputMode: this.keyboard()
             };
         }
         attr() {
@@ -4625,6 +4629,9 @@ var $;
         }
         selection_start() {
             return 0;
+        }
+        keyboard() {
+            return "text";
         }
         length_max() {
             return Infinity;
@@ -4698,7 +4705,7 @@ var $;
             event_change(next) {
                 if (!next)
                     return;
-                this.value(next.target.value);
+                this.value_changed(next.target.value);
                 this.selection_change(next);
             }
             hint_visible() {
@@ -4717,10 +4724,12 @@ var $;
                 const el = this.dom_node();
                 if (el !== this.$.$mol_dom_context.document.activeElement)
                     return;
-                this.selection([
+                const [from, to] = this.selection([
                     el.selectionStart,
                     el.selectionEnd,
                 ]);
+                el.selectionEnd = to;
+                el.selectionStart = from;
             }
             selection_start() {
                 return this.selection()[0];
--- node.test.js
+++ node.test.js
@@ -4548,7 +4548,10 @@ var $;
         selection(val) {
             if (val !== undefined)
                 return val;
-            return [];
+            return [
+                0,
+                0
+            ];
         }
         auto() {
             return [
@@ -4564,7 +4567,8 @@ var $;
                 spellcheck: this.spellcheck(),
                 autocomplete: this.autocomplete_native(),
                 selectionEnd: this.selection_end(),
-                selectionStart: this.selection_start()
+                selectionStart: this.selection_start(),
+                inputMode: this.keyboard()
             };
         }
         attr() {
@@ -4617,6 +4621,9 @@ var $;
         }
         selection_start() {
             return 0;
+        }
+        keyboard() {
+            return "text";
         }
         length_max() {
             return Infinity;
@@ -4690,7 +4697,7 @@ var $;
             event_change(next) {
                 if (!next)
                     return;
-                this.value(next.target.value);
+                this.value_changed(next.target.value);
                 this.selection_change(next);
             }
             hint_visible() {
@@ -4709,10 +4716,12 @@ var $;
                 const el = this.dom_node();
                 if (el !== this.$.$mol_dom_context.document.activeElement)
                     return;
-                this.selection([
+                const [from, to] = this.selection([
                     el.selectionStart,
                     el.selectionEnd,
                 ]);
+                el.selectionEnd = to;
+                el.selectionStart = from;
             }
             selection_start() {
                 return this.selection()[0];
