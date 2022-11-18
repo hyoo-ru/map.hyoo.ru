88a0fe3b5d18f6fe3cf53e90d815759b4126584b
nin-jin <nin-jin@users.noreply.github.com>
2022-11-18 07:12:14 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -6265,7 +6265,8 @@ var $;
                 ...super.field(),
                 src: this.uri(),
                 alt: this.title(),
-                loading: this.loading()
+                loading: this.loading(),
+                decoding: this.decoding()
             };
         }
         minimal_width() {
@@ -6279,6 +6280,9 @@ var $;
         }
         loading() {
             return "eager";
+        }
+        decoding() {
+            return "async";
         }
     }
     $.$mol_image = $mol_image;
--- web.js
+++ web.js
@@ -5939,7 +5939,8 @@ var $;
                 ...super.field(),
                 src: this.uri(),
                 alt: this.title(),
-                loading: this.loading()
+                loading: this.loading(),
+                decoding: this.decoding()
             };
         }
         minimal_width() {
@@ -5953,6 +5954,9 @@ var $;
         }
         loading() {
             return "eager";
+        }
+        decoding() {
+            return "async";
         }
     }
     $.$mol_image = $mol_image;
